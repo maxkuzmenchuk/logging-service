eb1afd2aab962405ece21c8836cdb3dbdec3cd76
Maxim Kuzmenchuk <46691880+maxkuzmenchuk@users.noreply.github.com>
2022-05-26 10:52:53 +0300

    init commit

---
+++ src/test/java/com/kuzmenchuk/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmenchuk.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
